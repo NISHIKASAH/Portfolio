1b8e1475b0429c1243b6e59e972b68125a3cbdf0
NISHIKASAH <81612543+NISHIKASAH@users.noreply.github.com>
2025-11-07 15:23:55 +0530

    Enhance experience section with new roles
    Updated experience details and formatting in Experience.js.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,9 +6,30 @@ const Experience = () => {
       <h2 className="mb-8 text-3xl text-white text-center">
         My <span>Experience</span>
       </h2>
-      <div className="mb-[20px] text-white relative bg-gray-700/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto">
+     <div className="mb-[20px] text-white relative bg-gray-700/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto pb-16">
+        <p>Mphasis</p>
+        <p className="text-gray-400">(March 2025 - Present)</p>
+        <p className="text-gray-500">
+         Developed key features including signup with captch, secure authentication, and login workflows using net mvc
+         Implemented unit tests to ensure reliability and correctness of core functionalities
+         Built booking operations with backend integration, handling data flow and process logic efficiently
+         Optimized end-to-end functionality to ensure seamless feature performance and system stability
+           Tech stack - dotnet , sql , react 
+        </p>
+      </div>
+     <div className="mb-[20px] text-white relative bg-gray-700/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto pb-16"">
+        <p>Klenty Private Limited </p>
+        <p className="text-gray-400">(December 2024 - March-2025)</p>
+        <p className="text-gray-500">
+             Developed and optimized UI components in a live SaaS product using Reactjs, TypeScript, and JavaScript
+             Built new features, enhanced performance, and fixed UI bugs for a seamless user experience
+             Contributed to backend tasks using Nodejs, Expressjs , and mongodb
+             Performed unit and manual testing, ensuring reliable releases in a fast-paced environment.
+        </p>
+      </div>
+      <div className="mb-[20px] text-white relative bg-gray-700/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto pb-16"">
         <p>HCL Techology</p>
-        <p className="text-gray-400">(Jan 2023-May 2023)</p>
+        <p className="text-gray-400">(January 2023-May 2023)</p>
         <p className="text-gray-500">
           As an intern at HCL Tech, I focused on frontend and database of backend development with mysql, crafting
           application to manage vehicle tracking and taxation for optimized
